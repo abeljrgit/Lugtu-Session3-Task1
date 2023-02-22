56e07026a953c163f7e1f67b9d52eccdae18d002
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-22 20:58:23 +0800

    🚑 fix: Key

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -5,18 +5,19 @@ export class ProfileCard extends Component {
   render() {
     return (
       <>
-        {this.props.listProfileData.map((profile) => {
+        {this.props.listProfileData.map((profile, index) => {
           return (
-            <Card sx={{ width: '100%', maxWidth: '400px', mt: 5, p: 3 }}>
+            <Card
+              key={index}
+              sx={{ width: '100%', maxWidth: '400px', mt: 5, p: 3 }}
+            >
               <Typography sx={{ fontSize: 24, lineHeight: 1 }}>
                 {profile.name}
               </Typography>
               <Typography sx={{ mt: 2 }}>
                 {profile.tellMeAboutYourself}
               </Typography>
-              <Typography sx={{ mt: 2 }}>
-                {new Date(profile.dateOfBirth).toISOString()}
-              </Typography>
+              <Typography sx={{ mt: 2 }}>{profile.dateOfBirth}</Typography>
               <Typography>{profile.email}</Typography>
               <Typography>{profile.contact}</Typography>
             </Card>
